0f17417c6acff431343f0f12663c98a7b2eee251
Sai Spandana Gali <60799194+span97@users.noreply.github.com>
2021-01-06 17:01:03 -0600

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,29 @@
+var cycle = 0; 
+
+var allBackgrounds = [
+                    "images/ollivanders.jpg",
+                     "images/green.jpg",
+                       "images/hogwarts.jpg",
+                       "images/class.jpg",
+                       "images/corridor.jpg",
+                       "images/quidditch.jpg"
+                       
+                       
+                       
+
+
+                       
+					    ]; 
+ 
+setInterval(function() { 
+	if (cycle < 6) { 
+        
+        document.getElementById("background").style.backgroundImage = "url('" + allBackgrounds[cycle] + "')"; 
+		cycle += 1; 
+	} else {  
+		cycle = 0; 
+    } 
+    
+    
+}, 10000); 
 

d2ad6fad9b47c3624d75782055cfc2f75e92afbb
Sai Spandana Gali <60799194+span97@users.noreply.github.com>
2021-01-07 13:53:40 -0600

    Update node.js

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ var cycle = 0;
 var allBackgrounds = [
                     "images/ollivanders.jpg",
                      "images/green.jpg",
-                       "images/hogwarts.jpg",
+                       "images/hogwarts.jpg",                      
                        "images/class.jpg",
                        "images/corridor.jpg",
                        "images/quidditch.jpg"
@@ -26,4 +26,42 @@ setInterval(function() {
     
     
 }, 10000); 
+
+//about me
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
 
